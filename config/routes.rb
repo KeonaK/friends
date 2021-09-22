c0aede8de76beaba82c310cbaf5393f6c4ae53ce
Keona Kirby <keonakirby@gmail.com>
2021-09-22 14:18:09 -0500

    added new links to navbar to add friends or go to friends page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  # creates crud routes
   resources :friends
   get 'home/about'
   # the home route created is now our root homepage
   root "home#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
